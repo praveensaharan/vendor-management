d8f11706ad1a79623d8aac170731f798b354a966
praveensaharan <200040109@iitb.ac.in>
2024-05-13 21:58:13 +0530

    browser

--- frontend/src/AuthContext.jsx
+++ frontend/src/AuthContext.jsx
@@ -1,4 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
+import {
+  getAuth,
+  onAuthStateChanged,
+  browserSessionPersistence,
+  setPersistence,
+} from "firebase/auth";
 import { auth } from "./firebase";
 
 const AuthContext = React.createContext();
@@ -9,22 +15,30 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        // User is signed in
-        setUser(authUser);
-      } else {
-        // User is signed out
-        setUser(null);
-      }
-    });
+    const auth = getAuth();
 
-    return unsubscribe;
+    // Set persistence
+    setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        // New sign-in will be persisted with session persistence
+        const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+          setUser(authUser);
+          setLoading(false);
+        });
+        return unsubscribe;
+      })
+      .catch((error) => {
+        console.error(error.code, error.message);
+        setLoading(false);
+      });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>
+      {!loading && children}
+    </AuthContext.Provider>
   );
 }
