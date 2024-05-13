623f2481ee327566d5c5436458cba524e9c490be
Praveen Saharan <84726895+praveensaharan@users.noreply.github.com>
2024-05-13 18:18:32 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjBztFNL_DZStaN1Eq7dXsnv7xU7u6-9Q",
-  authDomain: "jobs-cde29.firebaseapp.com",
-  projectId: "jobs-cde29",
-  storageBucket: "jobs-cde29.appspot.com",
-  messagingSenderId: "992854116233",
-  appId: "1:992854116233:web:e7b49e9e1ef91680250358",
-  measurementId: "G-7B1TKC1JVZ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
